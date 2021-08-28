e3813e4d84998ddaa301c406b17165cfd1fe4379
SMei201900 <smei201900@gmail.com>
2021-08-28 00:30:39 -0400

    Trying out video instructions

---
+++ src/Axiosatt.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+
+export default function Axiosatt() {
+	function one(response) {
+		alert(`weather is${response.data.main.temp}`);
+	}
+	let apiKey = "094780c710fa4efd669f0df8c3991927";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(one);
+}
